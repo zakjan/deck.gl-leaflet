a07809c3df7f9d83a6618bee77654dfbae189595
Jan Zak <zj@zakjan.cz>
2024-09-13 23:59:02 +0200

    Fix exposing fields

--- src/leaflet-layer.ts
+++ src/leaflet-layer.ts
@@ -3,15 +3,15 @@ import type { Deck, DeckProps } from '@deck.gl/core';
 import { createDeckInstance, updateDeckView } from './deck-utils.js';
 
 export class LeafletLayer extends L.Layer {  
-  #props: DeckProps;
-  #container: HTMLDivElement | undefined = undefined;
-  #deck: Deck | undefined = undefined;
-  #animate: boolean | undefined = undefined;
+  props: DeckProps;
+  _container: HTMLDivElement | undefined = undefined;
+  _deck: Deck | undefined = undefined;
+  _animate: boolean | undefined = undefined;
 
   constructor(props: DeckProps) {
     super();
 
-    this.#props = props;
+    this.props = props;
   }
 
   onAdd(): this {
@@ -20,29 +20,29 @@ export class LeafletLayer extends L.Layer {
       return this;
     }
 
-    this.#container = L.DomUtil.create('div');
-    this.#container.className = 'leaflet-layer';
+    this._container = L.DomUtil.create('div');
+    this._container.className = 'leaflet-layer';
     if (this.#getZoomAnimated()) {
-      L.DomUtil.addClass(this.#container, 'leaflet-zoom-animated');
+      L.DomUtil.addClass(this._container, 'leaflet-zoom-animated');
     }
 
-    pane.appendChild(this.#container);
-    this.#deck = createDeckInstance(this._map, this.#container, this.#deck, this.#props);
+    pane.appendChild(this._container);
+    this._deck = createDeckInstance(this._map, this._container, this._deck, this.props);
     this.#update();
 
     return this;
   }
 
   onRemove(_map: L.Map): this {
-    if (!this.#container || !this.#deck) {
+    if (!this._container || !this._deck) {
       return this;
     }
 
-    L.DomUtil.remove(this.#container);
-    this.#container = undefined;
+    L.DomUtil.remove(this._container);
+    this._container = undefined;
 
-    this.#deck.finalize();
-    this.#deck = undefined;
+    this._deck.finalize();
+    this._deck = undefined;
 
     return this;
   }
@@ -60,35 +60,35 @@ export class LeafletLayer extends L.Layer {
   }
 
   setProps(props: DeckProps): void {
-    Object.assign(this.#props, props);
+    Object.assign(this.props, props);
 
-    if (this.#deck) {
-      this.#deck.setProps(props);
+    if (this._deck) {
+      this._deck.setProps(props);
     }
   }
 
   pickObject(opts: Parameters<Deck['pickObject']>[0]): ReturnType<Deck['pickObject']> {
-    if (!this.#deck) {
+    if (!this._deck) {
       return null;
     }
 
-    return this.#deck.pickObject(opts);
+    return this._deck.pickObject(opts);
   }
 
   pickMultipleObjects(opts: Parameters<Deck['pickMultipleObjects']>[0]): ReturnType<Deck['pickMultipleObjects']> {
-    if (!this.#deck) {
+    if (!this._deck) {
       return [];
     }
 
-    return this.#deck.pickMultipleObjects(opts);
+    return this._deck.pickMultipleObjects(opts);
   }
 
   pickObjects(opts: Parameters<Deck['pickObjects']>[0]): ReturnType<Deck['pickObjects']> {
-    if (!this.#deck) {
+    if (!this._deck) {
       return [];
     }
 
-    return this.#deck.pickObjects(opts);
+    return this._deck.pickObjects(opts);
   }
 
   #getMap(): L.Map & { _animatingZoom: boolean, _getMapPanePos: () => L.Point } {
@@ -100,7 +100,7 @@ export class LeafletLayer extends L.Layer {
   }
 
   #update(): void {
-    if (!this.#container || !this.#deck) {
+    if (!this._container || !this._deck) {
       return;
     }
     if (this.#getMap()._animatingZoom) {
@@ -108,35 +108,35 @@ export class LeafletLayer extends L.Layer {
     }
 
     const size = this.#getMap().getSize();
-    this.#container.style.width = `${size.x}px`;
-    this.#container.style.height = `${size.y}px`;
+    this._container.style.width = `${size.x}px`;
+    this._container.style.height = `${size.y}px`;
 
     // invert map position
     const offset = this.#getMap()._getMapPanePos().multiplyBy(-1);
-    L.DomUtil.setPosition(this.#container, offset);
+    L.DomUtil.setPosition(this._container, offset);
 
-    updateDeckView(this.#deck, this._map);
+    updateDeckView(this._deck, this._map);
   }
 
   #pauseAnimation(): void {
-    if (!this.#deck) {
+    if (!this._deck) {
       return;
     }
 
-    if (this.#deck.props._animate) {
-      this.#animate = this.#deck.props._animate;
-      this.#deck.setProps({ _animate: false });
+    if (this._deck.props._animate) {
+      this._animate = this._deck.props._animate;
+      this._deck.setProps({ _animate: false });
     }
   }
 
   #unpauseAnimation(): void {
-    if (!this.#deck) {
+    if (!this._deck) {
       return;
     }
 
-    if (this.#animate) {
-      this.#deck.setProps({ _animate: this.#animate });
-      this.#animate = undefined;
+    if (this._animate) {
+      this._deck.setProps({ _animate: this._animate });
+      this._animate = undefined;
     }
   }
 
@@ -175,12 +175,12 @@ export class LeafletLayer extends L.Layer {
    * see L.Renderer._updateTransform https://github.com/Leaflet/Leaflet/blob/master/src/layer/vector/Renderer.js#L90-L105
    */
   #updateTransform(center: L.LatLng, zoom: number): void {
-    if (!this.#container) {
+    if (!this._container) {
       return;
     }
 
     const scale = this.#getMap().getZoomScale(zoom, this.#getMap().getZoom());
-    const position = L.DomUtil.getPosition(this.#container);
+    const position = L.DomUtil.getPosition(this._container);
     const viewHalf = this.#getMap().getSize().multiplyBy(0.5);
     const currentCenterPoint = this.#getMap().project(this.#getMap().getCenter(), zoom);
     const destCenterPoint = this.#getMap().project(center, zoom);
@@ -188,9 +188,9 @@ export class LeafletLayer extends L.Layer {
     const topLeftOffset = viewHalf.multiplyBy(-scale).add(position).add(viewHalf).subtract(centerOffset);
 
     if (L.Browser.any3d) {
-      L.DomUtil.setTransform(this.#container, topLeftOffset, scale);
+      L.DomUtil.setTransform(this._container, topLeftOffset, scale);
     } else {
-      L.DomUtil.setPosition(this.#container, topLeftOffset);
+      L.DomUtil.setPosition(this._container, topLeftOffset);
     }
   }
 }
